29b3138c5bbc2f40f91a160dc6bef9877f88d39e
Ahmad Alhussin <ahmad.alhusein2@gmail.com>
2023-07-02 15:36:18 +0300

    added github actions

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
 
 export default {
     input: "src/index.ts",
@@ -8,7 +9,19 @@ export default {
         dir: "dist",
         format: "esm"
     },
-    plugins: [typescript({
-        tsconfig: "./tsconfig.json"
-    }), resolve(), terser()],
+    plugins: [
+        typescript({
+            tsconfig: "./tsconfig.json"
+        }),
+        resolve(),
+        terser(),
+        copy({
+            targets: [
+                { src: './src/index.html', dest: 'dist' },
+                { src: './src/comic.html', dest: 'dist' },
+                { src: './src/index.css', dest: 'dist' },
+                { src: './static', dest: 'dist' },
+            ]
+        }),
+    ],
 };
